41a60b29303b7cc728f43dca30f4db1b36cdc7fa
jefer30039 <jefer30039@gmail.com>
2024-11-11 00:38:01 -0600

    Agregar clase Main con método main para iniciar la interfaz gráfica

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        //llama a la interfaz grafica
+        mainMenu interfaz = new mainMenu();
+        interfaz.setVisible(true);
+    }
+}
