d5b1405299654156514dcb469b197fd0c3c14e63
jefer30039 <jefer30039@gmail.com>
2024-11-15 03:31:16 -0600

    Agregar soporte para LookAndFeel Nimbus en la interfaz gráfica

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
     public static void main(String[] args) {
-        
-        //llama a la interfaz grafica
+        try {
+            // Establece el LookAndFeel nimbus
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        // Llama a la interfaz gráfica
         MainMenu interfaz = new MainMenu();
         interfaz.setVisible(true);
     }
